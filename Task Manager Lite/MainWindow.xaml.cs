df0fcac79f9bb85d2c164466c956b20a8ec83747
Андрій Грицюк <venganpro12@gmail.com>
2023-09-22 16:17:54 +0300

    almost done, whatever idk ¯\_(ツ)_/¯

--- Task Manager Lite/MainWindow.xaml.cs
+++ Task Manager Lite/MainWindow.xaml.cs
@@ -22,20 +22,62 @@ namespace Task_Manager_Lite
     /// </summary>
     public partial class MainWindow : Window
     {
-        Timer timer = new Timer(UpdateTasks);
-        public int UpdateInterval { get; set; } = 1000;
+        private Thread timer = new Thread(DoUpdate);
+        private static int UpdateInterval { get; set; } = 1000;
+        private static int UpdateIntervalOld { get; set; } = 1000;
         public static DataGrid g { get; set; } = new();
         public MainWindow()
         {
             InitializeComponent();
 
-            grid.ItemsSource = Process.GetProcesses();
-            timer.Change(10, UpdateInterval);
+            timer.Start();
+            grid.ItemsSource = g.Items;
         }
 
-        private static void UpdateTasks(object obj)
+        private static void DoUpdate(object obj)
         {
-            g.ItemsSource = Process.GetProcesses();
+            UpdateIntervalOld = UpdateInterval;
+            while (UpdateInterval == UpdateIntervalOld)
+            {
+                Thread.Sleep(UpdateInterval);
+                UpdateTasks();
+            }
+        }
+
+        private static void UpdateTasks()
+        {
+            Application.Current.Dispatcher.Invoke(() =>
+            { 
+                g.ItemsSource = Process.GetProcesses();
+            });
+        }
+
+        private void SetIntervalTo1(object sender, RoutedEventArgs e)
+        {
+            UpdateInterval = 1000;
+            timer = new Thread(DoUpdate);
+            timer.Start();
+        }
+
+        private void SetIntervalTo2(object sender, RoutedEventArgs e)
+        {
+            UpdateInterval = 2000;
+            timer = new Thread(DoUpdate);
+            timer.Start();
+        }
+
+        private void SetIntervalTo3(object sender, RoutedEventArgs e)
+        {
+            UpdateInterval = 3000;
+            timer = new Thread(DoUpdate);
+            timer.Start();
+        }
+
+        private void SetIntervalToValue(object sender, RoutedEventArgs e)
+        {
+            UpdateInterval = Convert.ToInt32(tbx.Text) * 1000;
+            timer = new Thread(DoUpdate);
+            timer.Start();
         }
     }
 }
