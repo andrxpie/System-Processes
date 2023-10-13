59301868a2126a080ef4e512086b239f3fcc9f5b
Андрій Грицюк <venganpro12@gmail.com>
2023-10-13 23:00:54 +0300

    Task - hw (1 - 3) done, working on 4, 5

--- Task - hw/MainWindow.xaml.cs
+++ Task - hw/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -21,11 +22,89 @@ namespace Task___hw
         private int symbols { get; set; } = 0;
         private int words { get; set; } = 0;
         private int asks { get; set; } = 0;
-        private int exclamatorys { get; set; } = 0;
+        private int exclamatories { get; set; } = 0;
+
+        private Task countSentences;
+        private Task countSymbols;
+        private Task countWords;
+        private Task countAsks;
+        private Task countExclamatories;
+
+        CancellationTokenSource cts;
+        CancellationToken token;
 
         public MainWindow()
         {
             InitializeComponent();
+
+            cts = new CancellationTokenSource();
+            token = cts.Token;
+
+            countSentences = new Task(CountSentences);
+            countSymbols = new Task(CountSymbols);
+            countWords = new Task(CountWords);
+            countAsks = new Task(CountAsks);
+            countExclamatories = new Task(CountExclamatories);
+        }
+
+        private void CountSentences()
+        {
+            Application.Current.Dispatcher.Invoke(new Action(() =>
+            {
+                if(sentensesCBX.IsChecked == true)
+                {
+                    sentences = text.Text.Count(x => x == '.' || x == '!' || x == '?');
+                    sentensesTBX.Text = "Sentences: " + sentences.ToString();
+                }
+            }));
+        }
+
+        private void CountSymbols()
+        {
+            Application.Current.Dispatcher.Invoke(new Action(() =>
+            {
+                if(symbolsCBX.IsChecked == true)
+                {
+                    symbols = text.Text.Count();
+                    symbolsTBX.Text = "Symbols: " + symbols.ToString();
+                }
+            }));
+        }
+
+        private void CountWords()
+        {
+            Application.Current.Dispatcher.Invoke(new Action(() =>
+            {
+                if(wordsCBX.IsChecked == true)
+                {
+                    words = text.Text.Count(x => x == ' ') + 1;
+                    wordsTBX.Text = "Words: " + words.ToString();
+                }
+            }));
+        }
+
+        private void CountAsks()
+        {
+            Application.Current.Dispatcher.Invoke(new Action(() =>
+            {
+                if(asksCBX.IsChecked == true)
+                {
+                    asks = text.Text.Count(x => x == '?');
+                    asksTBX.Text = "Asks: " + asks.ToString();
+                }
+            }));
+        }
+
+        private void CountExclamatories()
+        {
+            Application.Current.Dispatcher.Invoke(new Action(() =>
+            {
+                if(exclamatoriesCBX.IsChecked == true)
+                {
+                    exclamatories = text.Text.Count(x => x == '!');
+                    exclamatoriesTBX.Text = "Exclamatories: " + exclamatories.ToString();
+                }
+            }));
         }
 
         private void AnalysClick(object sender, RoutedEventArgs e)
@@ -36,34 +115,78 @@ namespace Task___hw
             asksTBX.Text = "Asks: ";
             exclamatoriesTBX.Text = "Exclamatories: ";
 
-            if(sentensesCBX.IsChecked == true)
+            try
             {
-                sentences = text.Text.Count(x => x == '.' || x == '!' || x == '?');
-                sentensesTBX.Text = "Sentences: " + sentences.ToString();
+                if(token.IsCancellationRequested == false)
+                {
+                    stop.IsEnabled = true; // time to stop
+
+                    countSentences.Start();
+                    countSymbols.Start();
+                    countWords.Start();
+                    countAsks.Start();
+                    countExclamatories.Start();
+
+                    stop.IsEnabled = false; // end of time
+                    
+                    MessageBox.Show("Done!");
+                }
+
+                restart.IsEnabled = true;
+                start.IsEnabled = false;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
             }
 
-            if(symbolsCBX.IsChecked == true)
-            {
-                symbols = text.Text.Count();
-                symbolsTBX.Text = "Symbols: " + symbols.ToString();
-            }
+        }
 
-            if(wordsCBX.IsChecked == true)
-            {
-                words = text.Text.Count(x => x == ' ') + 1;
-                wordsTBX.Text = "Words: " + words.ToString();
-            }
+        private void StopClick(object sender, RoutedEventArgs e)
+        {
+            cts.Cancel();
+            stop.IsEnabled = false;
+        }
 
-            if(asksCBX.IsChecked == true)
-            {
-                asks = text.Text.Count(x => x == '?');
-                asksTBX.Text = "Asks: " + asks.ToString();
-            }
+        private void RestartClick(object sender, RoutedEventArgs e)
+        {
+            cts = new CancellationTokenSource();
+            token = cts.Token;
 
-            if(exclamatoriesCBX.IsChecked == true)
+            countSentences = new Task(CountSentences);
+            countSymbols = new Task(CountSymbols);
+            countWords = new Task(CountWords);
+            countAsks = new Task(CountAsks);
+            countExclamatories = new Task(CountExclamatories);
+
+            sentensesTBX.Text = "Sentences: ";
+            symbolsTBX.Text = "Symbols: ";
+            wordsTBX.Text = "Words: ";
+            asksTBX.Text = "Asks: ";
+            exclamatoriesTBX.Text = "Exclamatories: ";
+
+            try
             {
-                exclamatorys = text.Text.Count(x => x == '!');
-                exclamatoriesTBX.Text = "Exclamatories: " + exclamatorys.ToString();
+                if(token.IsCancellationRequested == false)
+                {
+                    stop.IsEnabled = true; // time to stop
+
+                    countSentences.Start();
+                    countSymbols.Start();
+                    countWords.Start();
+                    countAsks.Start();
+                    countExclamatories.Start();
+
+                    stop.IsEnabled = false; // end of time
+
+                    MessageBox.Show("Done!");
+                }
+
+                restart.IsEnabled = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
             }
         }
     }
